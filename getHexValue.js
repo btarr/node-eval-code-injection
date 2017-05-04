e64bb3670e2a1916bc09015f0a0b817e3cfdb0e9
Bill Tarr <betarr13@gmail.com>
2017-05-04 01:12:26 -0400

    create vulnerable hex-value finder for colors

---
+++ getHexValue.js
@@ -0,0 +1,30 @@
+// user readline to get user input
+const readline = require('readline');
+
+// read from stdin, write to stdout
+const r = readline.createInterface({input: process.stdin, output: process.stdout});
+
+// NEVER call this function it is DANGEROUS
+// TODO: remove this dangerous function
+function secretFunction() {
+    console.log('How\'d you get in this function?')
+}
+
+const colorHexes = {
+    red : '#FF0000',
+    yellow : '#FFFF00',
+    blue : '#0000FF',
+    black : '#000000',
+    white : '#FFFFFF',
+    green : '#008000',
+    purple : '#800080',
+    orange: '#FFA500'
+};
+
+r.question('Enter a color to get its hex value: ', (statement) => {
+    // parse the variables from the input statement
+
+    // someone that doesn't know how to access an object property using a string value (e.g. colorHexes['blue']) might resort to this
+    console.log(eval('colorHexes.' + statement.toLowerCase()));
+    r.close();
+});
